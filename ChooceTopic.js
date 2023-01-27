974d62e6b0b59b9c6591ee346668d72cb03aebfe
ivanM-create <mikhaylyutin.ivan@gmail.com>
2023-01-27 14:26:38 +0300

    chenges

--- ChooceTopic.js
+++ ChooceTopic.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 
+const theme = fs.readFileSync(`${__dirname}topics/otter.txt`, 'utf-8');
+// class Topic {
+//   getTopic(file) {
+//     // return file.split('\n');
+//   }
+// }
+// const topic = new Topic();
+console.log(theme);
 
-class Topics{}
-
-module.exports = Topics;
+// module.exports = Topic;

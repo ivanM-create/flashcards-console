6cbca117a4bbb2c1b04de12988ce30cd7614fe55
Dmitriy-Ivanof <dmitrii_i@bk.ru>
2023-01-27 14:19:45 +0300

    question

---
+++ runer.js
@@ -0,0 +1,4 @@
+let readlineSync = require("readline-sync"),
+  animals = ["Nighthawk", "Otter", "Raccoon"],
+  index = readlineSync.keyInSelect(animals, "Choose the topic");
+console.log("Ok, " + animals[index] + " goes to your room.");
